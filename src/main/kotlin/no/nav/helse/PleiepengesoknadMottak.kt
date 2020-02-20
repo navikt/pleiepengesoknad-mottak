22f5363f311ba4bbc4f55671fb85cc8f2793b082
Andre Wolden <wolden.andre@gmail.com>
2020-02-20 13:44:07 +0100

    asdf

--- src/main/kotlin/no/nav/helse/PleiepengesoknadMottak.kt
+++ src/main/kotlin/no/nav/helse/PleiepengesoknadMottak.kt
@@ -29,7 +29,7 @@ import no.nav.helse.dusseldorf.ktor.jackson.dusseldorfConfigured
 import no.nav.helse.dusseldorf.ktor.metrics.MetricsRoute
 import no.nav.helse.dusseldorf.ktor.metrics.init
 import no.nav.helse.mottak.v1.*
-import no.nav.helse.mottak.v1.DittNavV1Api
+//import no.nav.helse.mottak.v1.DittNavV1Api
 import no.nav.helse.mottak.v1.DittNavV1Service
 import no.nav.helse.mottak.v1.SoknadV1Api
 import no.nav.helse.mottak.v1.SoknadV1KafkaProducer
@@ -132,11 +132,11 @@ fun Application.pleiepengesoknadMottak() {
                         soknadV1KafkaProducer = soknadV1KafkaProducer
                     )
                 )
-                DittNavV1Api(
-                    dittNavV1Service = DittNavV1Service(
-                        soknadV1KafkaProducer = soknadV1KafkaProducer
-                    )
-                )
+//                DittNavV1Api(
+//                    dittNavV1Service = DittNavV1Service(
+//                        soknadV1KafkaProducer = soknadV1KafkaProducer
+//                    )
+//                )
             }
         }
     }
